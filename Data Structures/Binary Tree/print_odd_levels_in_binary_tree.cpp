7895966a78a1342a724a240c3153c879d6da4152
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-19 05:13:20 +0530

    Update print_odd_levels_in_binary_tree.cpp

--- Data Structures/Binary Tree/print_odd_levels_in_binary_tree.cpp
+++ Data Structures/Binary Tree/print_odd_levels_in_binary_tree.cpp
@@ -1,180 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-/* Include header file*/
-#include<stdio.h>
-#include<stdlib.h>
-
-/* Define struct of BST */
-struct Tree{
-  int data;/*Data field*/
-  struct Tree*left_child,*right_child;/*Pointer field*/
+struct Tree {
+    int data;
+    struct Tree* left_child;
+    struct Tree* right_child;
 };
 
-/*Function declaration section*/
-void insert_node(struct Tree**,int);
-struct Tree* free_node(struct Tree*);
-void inorder_show(struct Tree*);
-void print_odd_level(struct Tree*,int);
+void insert_node(struct Tree** root, int value) {
+    struct Tree* new_node = (struct Tree*)malloc(sizeof(struct Tree));
 
-/*  Create and insert newly BST Node.*/
-void insert_node(struct Tree**root,int value){
+    if (new_node) {
+        new_node->left_child = NULL;
+        new_node->right_child = NULL;
+        new_node->data = value;
 
-  /* Create dynamic memory block and assign that address on new_node pointer */
-  struct Tree*new_node=(struct Tree*)malloc(sizeof(struct Tree));
-  
-    /* Check newly memory block are created or not*/
-  if(new_node)
-  {
-    /* Memory block are created successful */
+        if (*root) {
+            struct Tree* temp = *root;
 
-    /* Assign left_child and right_child pointer value to NULL */
-
-    new_node->left_child=NULL;
-
-    new_node->right_child=NULL;
-
-    /* Assign data value */
-    new_node->data=value;
-
-    /*Check BST root Pointer NULL or Not NULL*/ 
-    if(*root)
-    {
-
-      struct Tree*temp=*root;
-
-            /* find position to insert new_node.  */ 
-      while(temp)
-      {
-        if(temp->data>=value)
-        {
-          if(temp->left_child!=NULL)
-          {
-            /*temp pointer Move to left_child memory block */
-            temp=temp->left_child;
-          }
-          else
-          {
-            /*Assign newly created memory block address to temp->left_child pointer*/
-
-            temp->left_child=new_node;
-            /*Break loop*/
-            break;
-          }
+            while (temp) {
+                if (temp->data >= value) {
+                    if (temp->left_child != NULL) {
+                        temp = temp->left_child;
+                    } else {
+                        temp->left_child = new_node;
+                        break;
+                    }
+                } else {
+                    if (temp->right_child != NULL) {
+                        temp = temp->right_child;
+                    } else {
+                        temp->right_child = new_node;
+                        break;
+                    }
+                }
+            }
+        } else {
+            *root = new_node;
         }
-        else
-        {  
+    } else {
+        printf("\n Memory Overflow");
+    }
+}
 
-          if(temp->right_child!=NULL)
-          {
-            /*temp pointer Move to right_child memory block */
+void inorder_show(struct Tree* temp) {
+    if (temp) {
+        inorder_show(temp->left_child);
+        printf(" %d", temp->data);
+        inorder_show(temp->right_child);
+    }
+}
 
-            temp=temp->right_child;
-          }
-          else
-          {
-            /*Assign newly created memory block address to temp->right_child pointer*/
-            temp->right_child=new_node;
-            break;
-          }
+struct Tree* free_node(struct Tree* temp) {
+    if (temp) {
+        temp->left_child = free_node(temp->left_child);
+        temp->right_child = free_node(temp->right_child);
+        free(temp);
+        temp = NULL;
+    }
+    return NULL;
+}
+
+void print_odd_level(struct Tree* temp, int show) {
+    if (temp) {
+        print_odd_level(temp->left_child, show + 1);
+        print_odd_level(temp->right_child, show + 1);
+        if (show % 2 != 0) {
+            printf(" %d", temp->data);
         }
-      }
-      
     }
-    else{
+}
 
-      /*When root pointer is NULL that means no element of BST tree then Assign this newly created memory block to root pointer*/
-      *root=new_node;
+int main() {
+    struct Tree* root = NULL;
+
+    insert_node(&root, 150);
+    insert_node(&root, 70);
+    insert_node(&root, 30);
+    insert_node(&root, 90);
+    insert_node(&root, 240);
+    insert_node(&root, 110);
+    insert_node(&root, 153);
+    insert_node(&root, 650);
+    insert_node(&root, 20);
+    insert_node(&root, 10);
+    insert_node(&root, 15);
+    insert_node(&root, 660);
+    insert_node(&root, 80);
+
+    if (root) {
+        printf("\n BST Inorder Data Is: ");
+        inorder_show(root);
+
+        printf("\n Odd Level Nodes Is: ");
+        print_odd_level(root, 1);
+
+        root = free_node(root);
+    } else {
+        printf("\n Empty Tree ");
     }
-    
-  }
-  else {
 
-    /*Not created memory by malloc function*/
-   printf("\n memory Overflow");  
-
- }
-
+    return 0;
 }
-
-/* Show BST node data using this function */
-void inorder_show(struct Tree*temp){
-
-  if(temp){
-    inorder_show(temp->left_child);
-    printf("  %d",temp->data);
-    inorder_show(temp->right_child);
-  }
-  else
-    return;
-
-}
-
-
-/* Help of this function Free BST Nodes */
-struct Tree* free_node(struct Tree*temp){
-
-  if(temp){
-    temp->left_child=free_node((temp->left_child));
-    temp->right_child=free_node(temp->right_child);
-    free(temp);
-    temp=NULL;
-  }
-  return NULL;
-}
-
-
-/* Help of this function show BST Odd level data*/
-void print_odd_level(struct Tree*temp,int show){
-  /* If temp Not NULL */
-  if(temp)
-  {
-    print_odd_level(temp->left_child,show+1);
-    print_odd_level(temp->right_child,show+1);
-    if(show%2!=0){
-      /* Print odd level BST node */
-      printf(" %d",temp->data); 
-    } 
-  }
-}
-
-
-
-/*Main function*/
-int main(){ 
-    /*Start execution*/ 
-
-    /* BST structure pointer*/ 
-  struct Tree*root=NULL;
-
-    /*Suppose following data are inserted BST */ 
-    /* 150, 70, 30, 90, 240, 110, 153, 650, 20, 10, 15, 660, 80*/
-  insert_node(&root,150);
-  insert_node(&root,70);
-  insert_node(&root,30);
-  insert_node(&root,90);
-  insert_node(&root,240);
-  insert_node(&root,110);
-  insert_node(&root,153);
-  insert_node(&root,650);  
-  insert_node(&root,20);
-  insert_node(&root,10);
-  insert_node(&root,15);
-  insert_node(&root,660);
-  insert_node(&root,80);
-
-  if(root){
-    printf("\n BST Inorder Data Is :"); 
-    inorder_show(root);
-    
-    printf("\n Odd Level Nodes Is :");
-    print_odd_level(root,1);
-    /*Free BST Node*/
-    root=free_node(root);
-  }
-  else{
-    printf("\n Empty Tree ");
-  }
-  return 0; 
-
-}
-
-  
