4c1a99bd58a878b37ce10f3d1f53cf4a4056af9e
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-19 05:09:26 +0530

    Update NewmanConwaySequence.py

--- NewmanConwaySequence.py
+++ NewmanConwaySequence.py
@@ -1,7 +1,7 @@
 def newmanConwaySequence(n):
-    if n==1 or n==2:
+    if n == 1 or n == 2:
         return 1
-    return newmanConwaySequence(newmanConwaySequence(n-1))+newmanConwaySequence(n-newmanConwaySequence(n-1))
+    return newmanConwaySequence(newmanConwaySequence(n-1)) + newmanConwaySequence(n-newmanConwaySequence(n-1))
 
-n=int(input("Enter a number"))
+n = int(input("Enter a number: "))
 print(newmanConwaySequence(n))
