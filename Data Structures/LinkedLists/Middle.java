bc5d92a74625d188beb08d480aa948db5c78574b
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-18 18:05:49 +0530

    middle value of given linked list

--- Data Structures/LinkedLists/Middle.java
+++ Data Structures/LinkedLists/Middle.java
@@ -1,61 +1,35 @@
-import java.util.*;
-
 class Node {
-	int data;
-	Node next;
-	public Node() {
-		this.data = data;
-		this.next = null;
-	}
-	public Node(int d) {
-		this.data = d;
-	}
-}
+    int data;
+    Node next;
 
-class Linked {
-	Node head;
-	Linked() {
-		head = null;
-	}
-
-	void insertAtEnd(int data) {
-		Node n = new Node(data);
-		if(head == null) {
-			head = n;
-		} else {
-				Node temp;
-				for(temp = head; temp.next != null; temp = temp.next);
-				temp.next = n;
-				n.next = null;
-		}
-	}
-	void middleElement() {
-		Node temp;
-		int l = 1;
-		for(temp = head; temp.next != null; temp = temp.next) {
-			l++;
-		}
-		int pos = l/2;
-		int i;
-		for(temp = head, i = 0; temp.next.next != null && i <= pos - 1;temp = temp.next, i++);
-		System.out.println(temp.data);
-	}
-}
-
-class Middle {
-    public static void main(String[] args) {
-		Scanner Sc = new Scanner(System.in);
-		Linked ll = new Linked();
-        int size;
-        System.out.println("Enter the size:");
-        size = Sc.nextInt();
-        for( int s = 0; s < size; s++) {
-            System.out.println("Enter the data");
-            int data = Sc.nextInt();
-            ll.insertAtEnd(data);
-        }
-        System.out.print("Element in the middle of the linked list is: ");
-		ll.middleElement();
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
-}    
+}
 
+class LinkedList {
+    Node head;
+
+    void printMiddle() {
+        Node slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("The middle element is: " + slow.data);
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.next = new Node(3);
+        list.head.next.next.next = new Node(4);
+        list.head.next.next.next.next = new Node(5);
+
+        list.printMiddle();
+    }
+}
