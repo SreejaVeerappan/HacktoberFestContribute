d52bda131e4792ea9e85a39cc874eaa5a0782b8d
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-19 04:49:20 +0530

    Update StringStack.c

--- StringStack.c
+++ StringStack.c
@@ -1,38 +1,49 @@
-#include<stdio.h>
-#include<string.h>
- 
-int top=-1,stack[33];
- 
-void push(int item,int len)
-{
-	top=top+1;
-	stack[top]=item;
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STACK_SIZE 100
+
+int top = -1;
+char stack[MAX_STACK_SIZE];
+
+void push(char item) {
+    if (top >= MAX_STACK_SIZE - 1) {
+        printf("Stack overflow!\n");
+        return;
+    }
+    stack[++top] = item;
 }
- 
-void p()
-{
-	printf("%c",stack[top]);
-	top--;
+
+char pop() {
+    if (top < 0) {
+        printf("Stack underflow!\n");
+        return '\0'; // Return a null character to indicate underflow
+    }
+    return stack[top--];
 }
- 
- 
-void main()
-{
- 
-	char s[22];
-	int len,i;
-	printf("Enter string\n");
-	scanf("%s",s);
-	
-	len=strlen(s);
-	for(i=0;i<len;i++)
-	{
-	push(s[i],len);
-	}
-	printf("the reverse of string is\n");
-	for(i=0;i<len;i++)
-	{
-	p();
-	}
-	printf("\n");
+
+void reverseString(char* str) {
+    int len = strlen(str);
+
+    // Push characters onto the stack
+    for (int i = 0; i < len; i++) {
+        push(str[i]);
+    }
+
+    // Pop characters from the stack and store them back in the string
+    for (int i = 0; i < len; i++) {
+        str[i] = pop();
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    reverseString(str);
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
 }
