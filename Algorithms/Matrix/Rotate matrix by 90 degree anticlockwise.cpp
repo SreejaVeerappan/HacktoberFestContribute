4a9e72f2e250b2028f3a52727ca23340c8ae720d
SreejaVeerappan <144646423+SreejaVeerappan@users.noreply.github.com>
2024-10-19 04:54:50 +0530

    Update Rotate matrix by 90 degree anticlockwise.cpp

--- Algorithms/Matrix/Rotate matrix by 90 degree anticlockwise.cpp
+++ Algorithms/Matrix/Rotate matrix by 90 degree anticlockwise.cpp
@@ -1,27 +1,19 @@
-//efficient solution
-//Approach:-
-//1. Transpose the whole matrix
-//2. Reverse individual columns using two pointers approach
-
-class Solution
-{   
-    public:
-    //Function to rotate matrix anticlockwise by 90 degrees.
-    void rotateby90(vector<vector<int> >& mat, int n) 
-    { 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                swap(mat[i][j],mat[j][i]);  //transpose of a matrix
+class Solution {
+public:
+    void rotateby90(vector<vector<int>>& mat, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(mat[i][j], mat[j][i]);
             }
         }
-        
-        for(int i=0;i<n;i++){
-            int low = 0,high=n-1;
-            while(low<high){
-                swap(mat[low][i],mat[high][i]); //reverse individual columns
+
+        for (int i = 0; i < n; i++) {
+            int low = 0, high = n - 1;
+            while (low < high) {
+                swap(mat[low][i], mat[high][i]);
                 low++;
                 high--;
             }
         }
-    } 
+    }
 };
